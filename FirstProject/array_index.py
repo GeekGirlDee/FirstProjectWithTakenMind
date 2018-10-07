79c9d80b2747170b7374718dd7a89355d5e566f5
GeekGirlDee <dmabitle839@gmail.com>
2018-10-07 05:04:31 +0200

    numpy2

---
+++ FirstProject/array_index.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+arr = np.arange(0, 12)
+print arr
---
+++ FirstProject/scaler operations on array.py
@@ -0,0 +1,27 @@
+from __future__ import division
+import numpy as np
+
+print 5/2
+
+array1 = np.array([[1, 2, 3, 4],[5, 6, 7, 8]])
+print array1
+
+# multiplication
+array2 = array1 * array1
+print array2
+
+# exponential multiplication
+array3 = array1 ** 3
+print array3
+
+# subtraction
+array4 = array1 - array1
+print array4
+
+array5 = array2 - array1
+print array5
+
+# division / reciprocal
+array6 = 1/array1
+print array6
+
